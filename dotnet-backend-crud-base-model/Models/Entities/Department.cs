df97d37d7f3ec4e1aaba5732df901d3350588186
LumiNovryM <luminovrymekel@gmail.com>
2026-07-31 22:40:14 +0700

    Add project files.

---
+++ dotnet-backend-crud-base-model/Models/Entities/Department.cs
@@ -0,0 +1,13 @@
+﻿namespace dotnet_backend_crud_base_model.Models.Entities
+{
+    public class Department
+    {
+        public int Id { get; set; }
+
+        public string? DepartmentName { get; set; }
+
+        public string? Abbreviation { get; set; }
+
+        public ICollection<JobTitle> JobTitles { get; set; } = new List<JobTitle>();
+    }
+}
---
+++ dotnet-backend-crud-base-model/Models/Entities/JobTitle.cs
@@ -0,0 +1,15 @@
+﻿namespace dotnet_backend_crud_base_model.Models.Entities
+{
+    public class JobTitle
+    {
+        public int Id { get; set; }
+
+        public string? JobTitleName { get; set; }
+
+        public int? DepartmentId { get; set; }
+
+        public Department? Department { get; set; }
+
+        public ICollection<Employee> Employees { get; set; } = new List<Employee>();
+    }
+}
