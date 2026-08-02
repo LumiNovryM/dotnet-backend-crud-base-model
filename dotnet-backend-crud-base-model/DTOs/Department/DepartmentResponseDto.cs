0fd660aa9e7bc9bdd1ab56a4b1c7bb94afc7fde0
LumiNovryM <luminovrymekel@gmail.com>
2026-08-02 16:05:51 +0700

    Dev: Create Deparment & JobTitle Dtos Data

---
+++ dotnet-backend-crud-base-model/DTOs/Department/DepartmentResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace dotnet_backend_crud_base_model.DTOs.Department;
+
+public class DepartmentResponseDto
+{
+    public int Id { get; set; }
+
+    public string? DepartmentName { get; set; }
+}
---
+++ dotnet-backend-crud-base-model/DTOs/JobTitle/JobTitleResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace dotnet_backend_crud_base_model.DTOs.JobTitle;
+
+public class JobTitleResponseDto
+{
+    public int Id { get; set; }
+
+    public string? JobTitleName { get; set; }
+}
