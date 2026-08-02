d99b447805e70e62ea80215e1603aa326410b3c4
LumiNovryM <luminovrymekel@gmail.com>
2026-08-02 16:12:20 +0700

    Feat: Create Controller Deparment & Jobtitle

---
+++ dotnet-backend-crud-base-model/Controllers/DepartmentController.cs
@@ -0,0 +1,34 @@
+﻿using dotnet_backend_crud_base_model.Common;
+using dotnet_backend_crud_base_model.DTOs.Department;
+using dotnet_backend_crud_base_model.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_backend_crud_base_model.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class DepartmentController : ControllerBase
+{
+    private readonly IDepartmentService _service;
+
+
+    public DepartmentController(
+        IDepartmentService service)
+    {
+        _service = service;
+    }
+
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var departments = await _service.GetAllAsync();
+
+        return Ok(new ApiResponse<List<DepartmentResponseDto>>
+        {
+            Success = true,
+            Message = "Departments retrieved successfully.",
+            Data = departments
+        });
+    }
+}
---
+++ dotnet-backend-crud-base-model/Controllers/JobTitleController.cs
@@ -0,0 +1,37 @@
+﻿using dotnet_backend_crud_base_model.Common;
+using dotnet_backend_crud_base_model.DTOs.JobTitle;
+using dotnet_backend_crud_base_model.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_backend_crud_base_model.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class JobTitleController : ControllerBase
+{
+    private readonly IJobTitleService _service;
+
+
+    public JobTitleController(
+        IJobTitleService service)
+    {
+        _service = service;
+    }
+
+
+    [HttpGet]
+    public async Task<IActionResult> GetByDepartment(
+        [FromQuery] int departmentId)
+    {
+        var jobTitles =
+            await _service.GetByDepartmentIdAsync(departmentId);
+
+
+        return Ok(new ApiResponse<List<JobTitleResponseDto>>
+        {
+            Success = true,
+            Message = "Job titles retrieved successfully.",
+            Data = jobTitles
+        });
+    }
+}
